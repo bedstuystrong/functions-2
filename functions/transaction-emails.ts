d10783f02cbebe8bf774516e746f77507595092a
alex baldwin <ghostbaldwin@gmail.com>
2023-07-09 20:24:14 -0400

    init, airtable interface, adapt email handler
    airtable interface copied from https://github.com/bedstuystrong/ioby-downloader/blob/main/airtable.ts
    function adapted from https://github.com/bedstuystrong/firebase-functions/blob/master/functions/finance.js

---
+++ functions/transaction-emails.ts
@@ -0,0 +1,296 @@
+import type { Handler, HandlerEvent } from '@netlify/functions';
+import { simpleParser } from 'mailparser';
+import { type AddressObject, EmailAddress } from 'mailparser';
+import sendgridMail from '@sendgrid/mail';
+import pick from 'lodash/pick';
+import findKey from 'lodash/findKey';
+import { flow, get, map, first } from 'lodash/fp';
+
+import AirtableBase from 'lib/airtable';
+import parseMultipartForm from 'lib/multipart';
+
+sendgridMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const FUND_EMAIL_ADDRESS = 'fund@bedstuystrong.com';
+
+interface Email {
+  to: string;
+  from: string;
+  headers: string;
+  subject: string;
+  text: string;
+  html: string;
+}
+
+interface FinanceTransaction {
+  date: string; // ?
+  direction: 'In' | 'Out';
+  platform: 'Venmo' | 'Zelle' | 'Paypal' | 'Google Pay' | 'Cash App';
+  amount: number;
+  name: string;
+  note?: string;
+}
+
+const FINANCE_TRANSACTION_DIRECTIONS = {
+  In: 'recHqZivpo6j4T6On',
+  Out: 'reckW3l4mK8BCEBsd',
+};
+
+const createFinanceTransaction = async ({ direction, platform, amount, name, note, date }: FinanceTransaction) => {
+  const transactions = new AirtableBase('finance').table('transactions');
+
+  const directionID = FINANCE_TRANSACTION_DIRECTIONS[direction];
+
+  return await transactions.create({
+    direction: [directionID],
+    platform: platform,
+    amount: amount,
+    name: name,
+    notes: note,
+    date: date,
+  });
+}
+
+const PLATFORMS = {
+  venmo: {
+    from: 'venmo@venmo.com',
+    regex: /From: Venmo <venmo@venmo\.com>/,
+  },
+  zelle: {
+    from: '',
+    regex: /USAA Confirmation ID: [\d\n\r]+Zelle ID:/m,
+  },
+  amalgamated_zelle: {
+    from: 'noreply@online.amalgamatedbank.com',
+    regex: /From: Amalgamated Bank <noreply@online\.amalgamatedbank\.com>/,
+  },
+  paypal: {
+    from: 'service@paypal.com',
+    regex: /From: service@paypal\.com <service@paypal\.com>/,
+  },
+  googlepay: {
+    from: 'googlepay-noreply@google.com',
+    regex: /From: Google Pay <googlepay-noreply@google\.com>/,
+  },
+  cashapp: {
+    from: 'cash@square.com',
+    regex: /From: Cash App <cash@square\.com>/
+  },
+};
+
+const getFirstEmailAddressFromHeader = (header: AddressObject) => flow(
+  get('value'),
+  map((v: EmailAddress) => v.address),
+  first,
+)(header);
+
+const detectPaymentPlatform = (email: Email, { isAutoForwarded }: { isAutoForwarded: boolean }) => {
+  if (isAutoForwarded) {
+    return findKey(PLATFORMS, (platform) => {
+      if (platform.from) {
+        return platform.from === email.from;
+      } else {
+        return platform.regex.test(email.text);
+      }
+    });
+  } else {
+    return findKey(PLATFORMS, platform => platform.regex.test(email.text));
+  }
+};
+
+interface TransactionDetails extends Omit<FinanceTransaction, 'amount' | 'date'> {
+  amount: string;
+}
+
+const extractPaymentDetails = (platform: string, email: Email) => {
+  const details = {} as TransactionDetails;
+
+  switch (platform) {
+    case 'venmo': {
+      details.platform = 'Venmo';
+      const fromMatches = email.subject.match(/(?:Fwd:\s)?(.+) paid you (\$[\d.,]+)/);
+      const toMatches = email.subject.match(/You paid (.+) (\$[\d.,]+)/);
+
+      if (fromMatches) {
+        details.direction = 'In';
+        details.name = fromMatches[1];
+        details.amount = fromMatches[2];
+      } else if (toMatches) {
+        details.direction = 'Out';
+        details.name = toMatches[1];
+        details.amount = '-' + toMatches[2];
+      }
+      break;
+    }
+    case 'zelle': {
+      details.platform = 'Zelle';
+      const fromMatches = email.text.match(/tell you that (.*) sent ([$\d.,]+) with/);
+      const toMatches = email.text.match(/that you sent (\$[\d.,]+) to (.*) on/);
+
+      if (fromMatches) {
+        details.direction = 'In';
+        details.name = fromMatches[1];
+        details.amount = fromMatches[2];
+      } else if (toMatches) {
+        details.direction = 'Out';
+        details.name = toMatches[2];
+        details.amount = '-' + toMatches[1];
+      }
+      break;
+    }
+    case 'amalgamated_zelle': {
+      details.platform = 'Zelle';
+      const fromMatches = email.subject.match(/Notification - (.*) sent you (\$[\d.,]+)/);
+      const toMatches = email.subject.match(/Notification - Your \s?(\$[\d.,]+) to (.*) was sent/);
+      const noteMatches = email.html.match(/<p class="memo" [^>]+>(.*)<\/p>/m);
+
+      if (fromMatches) {
+        details.direction = 'In';
+        details.name = fromMatches[1];
+        details.amount = fromMatches[2];
+      } else if (toMatches) {
+        details.direction = 'Out';
+        details.name = toMatches[2];
+        details.amount = '-' + toMatches[1];
+      }
+
+      if (noteMatches) {
+        details.note = noteMatches[1];
+      }
+
+      break;
+    }
+    case 'paypal': {
+      details.platform = 'Paypal';
+      const text = email.html.replace(/(<([^>]+)>)/ig, '');
+      const fromMatches = text.match(/(.*) sent you ([$\d.,]+)/);
+      const toMatches = text.match(/You sent ([$\d.,]+) USD to (.*)/);
+      const noteMatches = text.match(/\[image: quote\] (.*) \[image: quote\]/);
+
+      if (fromMatches) {
+        details.direction = 'In';
+        details.name = fromMatches[1];
+        details.amount = fromMatches[2];
+      } else if (toMatches) {
+        details.direction = 'Out';
+        details.name = toMatches[2];
+        details.amount = '-' + toMatches[1];
+      }
+
+      if (noteMatches) {
+        details.note = noteMatches[1];
+      }
+
+      break;
+    }
+    case 'googlepay': {
+      details.platform = 'Google Pay';
+      const fromMatches = email.subject.match(/(.*) sent you ([$\d.,]+)/);
+      const toMatches = email.subject.match(/You sent ([^$]+) ([$\d.,]+)/);
+
+      if (fromMatches) {
+        details.direction = 'In';
+        details.name = fromMatches[1];
+        details.amount = fromMatches[2];
+      } else if (toMatches) {
+        details.direction = 'Out';
+        details.name = toMatches[1];
+        details.amount = '-' + toMatches[2];
+      }
+
+      break;
+    }
+    case 'cashapp': {
+      details.platform = 'Cash App';
+      const fromMatches = email.subject.match(/(?:Fwd:\s)?(.+) sent you (\$[\d.,]+)(?: for (.*))?/);
+      const toMatches = email.subject.match(/You sent (\$[\d.,]+) to (.*)/);
+      const toAcceptedMatches = email.subject.match(/(?:Fwd: )?(.*) just accepted the (\$[\d.,]+) you sent for (.*)/);
+
+      if (fromMatches) {
+        details.direction = 'In';
+        details.name = fromMatches[1];
+        details.amount = fromMatches[2];
+        details.note = fromMatches[3];
+      } else if (toMatches) {
+        details.direction = 'Out';
+        details.amount = '-' + toMatches[1];
+
+        const split = toMatches[2].split(/ for (.+)/);
+        if (split.length > 1) {
+          details.name = split[0];
+          details.note = split[1];
+        } else {
+          details.name = split[0];
+        }
+      } else if (toAcceptedMatches) {
+        details.direction = 'Out';
+        details.name = toAcceptedMatches[1];
+        details.amount = '-' + toAcceptedMatches[2];
+        details.note = toAcceptedMatches[3];
+      }
+
+      break;
+    }
+    default:
+      throw new Error(`Unhandled payment platform: ${platform}`);
+  }
+
+  return details;
+};
+
+const handler: Handler = async (event: HandlerEvent) => {
+  const fields = await parseMultipartForm(event);
+  const email = pick(fields, ['to', 'from', 'headers', 'subject', 'text', 'html']) as Email;
+  const parsed = await simpleParser(email.headers);
+
+  const date = parsed.headers.get('date');
+  email.to = getFirstEmailAddressFromHeader(parsed.headers.get('to') as AddressObject);
+
+  if (email.to.split('@')[0] !== 'funds' && email.to !== FUND_EMAIL_ADDRESS) {
+    // Log and do nothing
+    console.warn('Received email for user other than funds@', email);
+    return {
+      statusCode: 200,
+      body: 'OK',
+    };
+  }
+
+  const isAutoForwarded = email.to === FUND_EMAIL_ADDRESS;
+
+  email.from = getFirstEmailAddressFromHeader(parsed.headers.get('from') as AddressObject);
+
+  if (!isAutoForwarded && email.from !== FUND_EMAIL_ADDRESS) {
+    // Log and do nothing
+    console.warn('Received email from unauthorized forwarder', email);
+    return {
+      statusCode: 200,
+      body: 'OK',
+    };
+  }
+
+  const paymentPlatform = detectPaymentPlatform(email, { isAutoForwarded });
+  if (!paymentPlatform) {
+    // todo error
+    console.error(email);
+    await sendgridMail.send({
+      from: 'finance-script@em9481.mail.bedstuystrong.com',
+      to: FUND_EMAIL_ADDRESS,
+      subject: 'Error parsing payment email',
+      text: `Timestamp: ${(new Date()).toString()}`
+    });
+    throw new Error('Couldn\'t detect payment platform');
+  }
+
+  const details = extractPaymentDetails(paymentPlatform, email);
+
+  // @ts-ignore FIXME
+  await createFinanceTransaction(Object.assign(details, { date }));
+
+  return {
+    statusCode: 200,
+    body: 'OK',
+  };
+
+};
+
+export { handler };
---
+++ lib/airtable.ts
@@ -0,0 +1,136 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import Airtable from 'airtable';
+import _ from 'lodash';
+import invariant from 'tiny-invariant';
+
+import { isNodeError } from './util';
+
+interface AirtableTableSchema {
+  [key: string]: string;
+}
+
+interface AirtableTableConfig {
+  key: string;
+  name: string;
+  schema: AirtableTableSchema;
+}
+
+interface AirtableBaseConfig {
+  id: string;
+  key: string;
+  tables: AirtableTableConfig[];
+}
+
+interface AirtableConfig {
+  bases: AirtableBaseConfig[];
+}
+
+interface NormalizedAirtableRecord {
+  id: string;
+  _record: Airtable.Record<Airtable.FieldSet>;
+  _meta?: Record<string, any> | null;
+  [key: string]: any;
+}
+
+interface RecordCreateOptionalParameters {
+  typecast?: boolean;
+}
+
+export default class AirtableBase {
+  client: Airtable;
+  config: AirtableBaseConfig;
+  _base: Airtable.Base;
+
+  constructor(baseKey: string, config?: AirtableConfig) {
+    if (!config) {
+      try {
+        const configFile = fs.readFileSync(path.resolve(process.cwd(), 'airtable.config.json'), 'utf8');
+        config = JSON.parse(configFile);
+      } catch (error) {
+        const isENOENT = isNodeError(error) && error.code === 'ENOENT';
+        const errorMessage = isENOENT ? `Missing Airtable config: Can't find default config file airtable.config.json` : error;
+        throw new Error(errorMessage);
+      }
+    }
+
+    this.client = new Airtable({
+      apiKey: process.env.AIRTABLE_API_KEY,
+    });
+
+    this.config = _.find(config.bases, ['key', baseKey]);
+    invariant(this.config, `could not find base with key "${baseKey}" in config`);
+
+    this._base = this.client.base(this.config.id);
+  }
+
+  table = (tableKey: string) => {
+    return new AirtableTable(this, tableKey);
+  }
+}
+
+class AirtableTable {
+  client: Airtable;
+  config: AirtableTableConfig;
+  base: AirtableBase;
+  _table: Airtable.Table<Airtable.FieldSet>;
+
+  constructor(base: AirtableBase, tableKey: string) {
+    this.base = base;
+    this.client = this.base.client;
+
+    this.config = _.find(this.base.config.tables, ['key', tableKey]);
+    invariant(this.config, `could not find table with key "${tableKey}" in base "${this.base.config.key}" config`);
+
+    this._table = this.base._base(this.config.name);
+  }
+
+  create = async (
+    data: Record<string, any> | Record<string, any>[],
+    params?: RecordCreateOptionalParameters
+  ): Promise<NormalizedAirtableRecord | NormalizedAirtableRecord[]> => {
+    const payload = Array.isArray(data) ? data.map((fields) => ({
+      fields: this.denormalize(fields),
+    })) : this.denormalize(data);
+    // @ts-ignore-error FIXME
+    const result = await this._table.create(payload, {
+      typecast: true,
+      ...params,
+    });
+    // @ts-ignore-error FIXME
+    return Array.isArray(result) ? result.map(this.normalize) : this.normalize(result);
+  }
+
+  normalize = (record: Airtable.Record<Airtable.FieldSet>): NormalizedAirtableRecord => {
+    const fields = { ...record.fields };
+    const invertedSchema = _.invert(this.config.schema);
+
+    const recordBase = { id: record.id, _record: record } as NormalizedAirtableRecord;
+    if (fields._meta) {
+      recordBase._meta = JSON.parse(fields._meta as string);
+      Reflect.deleteProperty(fields, '_meta');
+    }
+
+    const normalizedFields = _.mapKeys(fields, (_value, key) => (invertedSchema[key] || key));
+
+    return _.assign(
+      recordBase,
+      _.mapValues(this.config.schema, () => null), // Airtable.Record doesn't include empty fields
+      normalizedFields,
+    );
+  };
+
+  denormalize = (object: Record<string, any>) => {
+    // Remove null keys and map back to original schema
+    const denormalized = _.mapKeys(
+      _.pickBy(object, (value) => !_.isNull(value)),
+      (_value, key) => (this.config.schema[key] || key)
+    );
+
+    if (denormalized._meta) {
+      denormalized._meta = JSON.stringify(denormalized._meta);
+    }
+
+    return denormalized;
+  };
+}
