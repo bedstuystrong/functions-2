f755bb4933439136715d84c520ed58072d34e9a4
alex baldwin <ghostbaldwin@gmail.com>
2024-11-09 03:35:25 -0500

    email template preview server

---
+++ dev-server.js
@@ -0,0 +1,28 @@
+import path from 'node:path';
+import Fastify from 'fastify';
+import { Eta } from 'eta';
+import juice from 'juice';
+
+const server = Fastify();
+
+const eta = new Eta({ views: path.resolve(process.cwd(), 'templates') });
+
+server.get('/email/:templateName', async function handler(request, reply) {
+  const { templateName } = request.params;
+
+  let html = await eta.renderAsync(templateName, {  });
+
+  if (request.query.inline) {
+    html = juice(html, { removeStyleTags: false })
+  }
+
+  return reply.type('text/html').send(html);
+});
+
+try {
+  await server.listen({ port: process.env.PORT || 3001 });
+} catch (err) {
+  server.log.error(err);
+  process.exit(1);
+}
+
